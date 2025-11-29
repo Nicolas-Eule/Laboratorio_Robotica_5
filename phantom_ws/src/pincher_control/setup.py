6438f217d9702ed907fa04a70bd428b69ff7cd23
ElJoho <johalopezari@unal.edu.co>
2025-11-28 23:16:01 -0500

    Se añadio control_servo.py del laboratorio de RVIZ

--- phantom_ws/src/pincher_control/setup.py
+++ phantom_ws/src/pincher_control/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'pincher_control'
 
@@ -8,8 +10,10 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+        glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +28,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'template_control_servo = pincher_control.template_control_servo:main',
             'control_servo = pincher_control.control_servo:main',
             'follow_joint_trajectory = pincher_control.follow_joint_trajectory_node:main',
         ],
